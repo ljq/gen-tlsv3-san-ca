7177038082b654c3e4e925ae05d9bf4702be1ac4
刘建秋 <stakgo@163.com>
2021-03-16 16:53:32 +0800

    fix

--- gen-tlsv3-san-ca.sh
+++ gen-tlsv3-san-ca.sh
@@ -56,7 +56,7 @@ default_md = sha256
 distinguished_name = req_distinguished_name
 req_extensions = v3_req
 
-[req_distinguished_name]clear
+[req_distinguished_name]
 
 [v3_req]
 basicConstraints = CA:FALSE
